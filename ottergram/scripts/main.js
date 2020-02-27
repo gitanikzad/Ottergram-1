6bb147bfaf98b2e319608b7c216759ac68a2f837
banana_lord kasravi <gitanikzad@csu.fullerton.edu>
2020-02-26 16:10:39 -0800

    initial commit

---
+++ ottergram/scripts/main.js
@@ -0,0 +1,78 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR='[data-image-role="frame"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS='hidden-detail';
+var TINY_EFFECT_CLASS='is-tiny';
+var ESC_KEY=27;
+function setDetails(imageUrl,titleText){
+    'use strict';
+    var detailImage=document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src',imageUrl);
+    var detailTitle=document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent=titleText;
+   
+
+}
+function imageFormThumb(thumbnail){
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+function titleFromThumb(thumbnail){
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+function setDetailsFromThumb (thumbnail){
+    'use strict';
+    setDetails(imageFormThumb(thumbnail), titleFromThumb(thumbnail));
+}
+function addThumbClickHandler(thumb)
+{
+    'use strict';
+    thumb.addEventListener('click', function(event){
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+        showDetails();
+    });
+
+}
+function getThumbnailsArray(){
+    'use strict';
+    var thumbnails=document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var getThumbnailsArray=[].slice.call(thumbnails);
+    return getThumbnailsArray;
+}
+function hideDetails(){
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails(){
+    'use strict';
+    var frame=document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function (){
+    frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+function addkeyPressHandler(){
+    'use strict';
+    document.body.addEventListener('keyup', function (event) {
+        
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode===ESC_KEY){
+            hideDetails();
+        }
+    });
+}
+
+function initializeEvents(){
+    'use strict';
+    var thumbnails=getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+    addkeyPressHandler();
+}
+initializeEvents();
